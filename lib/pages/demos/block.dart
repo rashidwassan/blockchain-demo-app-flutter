3f723f24dd8c028ddbef4ad0ff5bf4fc8fc4eff6
rashidwassaan <rashidwassaan@gmail.com>
2022-04-09 20:22:40 +0500

    added block page with complete functionality

--- lib/pages/demos/block.dart
+++ lib/pages/demos/block.dart
@@ -14,9 +14,19 @@ class BlockPage extends StatefulWidget {
 }
 
 class _HashPageState extends State<BlockPage> {
+  getHash() {
+    setState(() {
+      _hashWithNonce = CryptoUtils.getSHA256WithDifficulty(
+        data: _contentController.text,
+        difficulty: difficulty.toInt(),
+      );
+    });
+  }
+
   final TextEditingController _contentController = TextEditingController();
-  HashWithNonce _hashWithNonce = HashWithNonce('', 1);
-  int difficulty = 1;
+  HashWithNonce _hashWithNonce = HashWithNonce('', 0);
+
+  double difficulty = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,54 +34,81 @@ class _HashPageState extends State<BlockPage> {
         body: Padding(
           padding: const EdgeInsets.all(24.0),
           child: Center(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 12,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.teal, borderRadius: BorderRadius.circular(8)),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
-                child: Text(
-                  'Nonce: ${_hashWithNonce.nonce.toString()}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 12,
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Text(
-                'Enter the content of the block',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              DataInputField(
-                  title: 'Data',
-                  controller: _contentController,
-                  onChanged: (value) {
-                    setState(() {
-                      _hashWithNonce = CryptoUtils.getSHA256WithDifficulty(
-                        data: _contentController.text,
-                        difficulty: difficulty,
-                      );
-                    });
-                  }),
-              const SizedBox(
-                height: 24,
-              ),
-              Text(
-                'Hash:\n${_hashWithNonce.hash}',
-                textAlign: TextAlign.center,
-              ),
-            ],
-          )),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.circular(8)),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+                  child: Text(
+                    'Nonce: ${_hashWithNonce.nonce.toString()}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'Enter the content of the block',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                DataInputField(
+                    title: 'Data',
+                    controller: _contentController,
+                    onChanged: (value) {
+                      // setState(() {
+                      //   _hashWithNonce = CryptoUtils.getSHA256WithDifficulty(
+                      //     data: _contentController.text,
+                      //     difficulty: difficulty.toInt(),
+                      //   );
+                      // });
+                    }),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'Hash:\n${_hashWithNonce.hash}',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Spacer(),
+                const Text('Select Difficulty:'),
+                Slider(
+                    min: 1,
+                    max: 15,
+                    divisions: 14,
+                    value: difficulty,
+                    onChanged: (v) {
+                      setState(() {
+                        difficulty = v;
+                      });
+                    }),
+                Text(difficulty.toInt().toString()),
+                const Spacer(),
+                MaterialButton(
+                  onPressed: getHash,
+                  child: const Text(
+                    'Mine',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                )
+              ],
+            ),
+          ),
         ));
   }
 }
